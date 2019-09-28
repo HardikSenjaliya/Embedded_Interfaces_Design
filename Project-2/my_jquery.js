c848033c58f8fa90e43ba7e204efa1fbf75f9721
Hardik Senjaliya <hardyksenjaliya243@gmail.com>
2019-09-28 12:55:25 -0600

    addig HTML client with tornado webserver

---
+++ Project-2/my_jquery.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+	var ws;
+	var host = "localhost";
+	var port = "8888";
+	var uri = "/ws";
+
+ 	 // create websocket instance
+ 	 ws = new WebSocket("ws://" + host + ":" + port + uri);
+
+ 	 // Handle incoming websocket message callback
+ 	 ws.onmessage = function(evt) {
+ 	 	var temp = evt.data;
+ 	 	$("#output-temp").val(temp);
+ 	 };
+
+ 	 // Close Websocket callback
+ 	 ws.onclose = function(evt) {
+ 	 };
+
+ 	 // Open Websocket callback
+  	ws.onopen = function(evt) { 
+  	};	
+
+	$(".get-temperature").click(function() {
+		//$("#output-temp").val("400");
+		ws.send("500").val();
+	});
+
+
+
+});
